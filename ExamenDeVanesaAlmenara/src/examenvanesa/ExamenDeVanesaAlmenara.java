5537a9585d0cf2218058f98212504e6d6fed41ec
usuario <usuario@localhost>
2018-04-19 09:45:48 +0000

    estado inicial

--- ExamenDeVanesaAlmenara/src/examenvanesa/ExamenDeVanesaAlmenara.java
+++ ExamenDeVanesaAlmenara/src/examenvanesa/ExamenDeVanesaAlmenara.java
@@ -3,7 +3,7 @@ package examenvanesa;
 public class ExamenDeVanesaAlmenara {
 
 	public static void main(String[] args) {
-		System.out.println("La fecha de hoy es: 19/04/18");
+		System.out.println("La fecha de hoy es: 19/04/2018");
 
 	}
 
